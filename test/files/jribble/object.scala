2eb21fdc9ac87644aa12743e8dafd06019e3081d
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-05-25 22:59:22 +0200

    Made object test-case more robust.
    Added members to an object in a test-case for object
    compilation. This way we test how members of an
    object are compiled to jribble.
    
    Change-Id: I5c7e10ff77c61195845acb574aaa244066603eb8
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- test/files/jribble/object.scala
+++ test/files/jribble/object.scala
@@ -1,3 +1,7 @@
 object TestObject {
   //this test is about testing how scala object is translated into jribble
+  val field: Int = 0
+  var mutableField: Int = 0
+
+  def method(x: Int): Int = x
 }
