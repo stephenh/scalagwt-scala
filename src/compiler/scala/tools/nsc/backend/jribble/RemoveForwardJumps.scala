6aa4d736739e8a83cea076b2da78146e0c607b43
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-07-06 15:26:27 +0200

    Remove hacks for forward jumps and admit we don't handle them.
    Previous logic for handling some cases of forward jumps
    was very, very fragile and was causing more harm than
    good.
    
    It turns out that handling of forward jumps is not very
    critical because only a few classes in Scala library
    depend on them.
    
    Therefore, current strategy is to detect forward jumps
    and throw away body of a method enclosing the jump
    completely. We just replace it with an statement
    throwing an exception. This will provide us a neccesary
    info at the runtime just in case we've run into code
    fragment that depends on forward jumps.
    
    Change-Id: I313ac5772ffc9f3e0fb4846cab143b7948e57dfb
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- src/compiler/scala/tools/nsc/backend/jribble/RemoveForwardJumps.scala
+++ src/compiler/scala/tools/nsc/backend/jribble/RemoveForwardJumps.scala
@@ -9,27 +9,24 @@
 package scala.tools.nsc
 package backend.jribble
 import scala.collection.mutable
-import scala.tools.nsc.transform.Transform
+import scala.tools.nsc.transform.{Transform, TypingTransformers}
 
 /**
- * Forward jumps to LabelDefs cannot be expressed in jribble so they need to be transformed into method calls.
+ * Forward jumps to LabelDefs cannot be expressed in jribble so rewrite methods containing them into stubs.
  *
  * This transformation has two phases (executed for each Template):
  * 1. It scans using FindLabelForwardJumpsTraverser the tree to find list of all symbols corresponding to
  *    LabelDefs that we found a forward jump to. This phase doesn't collect symbols for LabelDefs that
  *    have jump instructions only in scope of LabelDefs. Those can be translated to while loop with label and
  *    jumps can be translated to continue <label>; instructions.
- * 2. For all LabelDefs that have forward jumps we lift them to top level, final, private methods and we put
- *    a call to that method in place of LabelDef. All jumps to that label are being translated to method calls too.
+ * 2. Set of symbols from 1. is mapped to Set of symbols correspodning to methods enclosing LabelDefs.
+ * 3. For each method from 2. we rewrite it's body to call sys.error(...) method (effectively: throw an exception).
  *
- * The reason why we don't translate all jumps to method calls is that they are less efficient that while loops
- * and continue instructions. This is especially important for tailcall optimizations that would be broken
- * (essentially inverted) if we would translate all jumps into method calls.
- *
- * Our hope is that forward jumps are only emitted by pattern matching logic which won't be hurt by this transformation
- * because we don't expect deep recursive calls.
+ * TODO(grek): This is really a temporary work-around until we find proper solution to deal with forward jumps.
+ * It turns out that forward jumps are not used that often in real Scala programs so we can get non-trivial, Scala
+ * programs running without dealing with (complex) issue of forward jumps.
  */
-trait RemoveForwardJumps extends Transform with JribbleNormalization {
+trait RemoveForwardJumps extends Transform with TypingTransformers with JribbleNormalization {
   val global: Global
   import global._
 
@@ -41,49 +38,30 @@ trait RemoveForwardJumps extends Transform with JribbleNormalization {
   /**
    * The main transformer of this phase.
    */
-  private class Trans(cunit: CompilationUnit) extends Transformer {
+  private class Trans(cunit: CompilationUnit) extends TypingTransformer(cunit) {
 
-    var forwardJumps = Set.empty[Symbol]
-
-    val methodsForLabels = mutable.Map.empty[Symbol, DefDef]
+    var methodsToBeRewritten = Set.empty[Symbol]
 
     var currentTemplateSym: Symbol = NoSymbol
 
     override def transformTemplate(tree: Template): Template = {
       currentTemplateSym = tree.symbol
-      val savedForwardJumps = forwardJumps
-      val traverser = new FindLabelForwardJumpsTraverser
-      traverser.traverse(tree)
-      forwardJumps = traverser.forwardJumps
-      val newTree@Template(parents, self, body) = super.transformTemplate(tree)
-      val newTemplate = treeCopy.Template(newTree, parents, self, body ++ methodsForLabels.values)
-      methodsForLabels.clear()
-      newTemplate
+      val findForwardJumps = new FindLabelForwardJumpsTraverser
+      findForwardJumps.traverse(tree)
+      val forwardJumps = findForwardJumps.forwardJumps
+      def enclMethod(s: Symbol): Symbol = if (s.isMethod & !s.isLabel) s else enclMethod(s.owner)
+      methodsToBeRewritten = forwardJumps.map(enclMethod)
+      methodsToBeRewritten foreach { s =>
+        cunit.warning(s.pos, "Forward jump detected, we'll be rewritten to sys.error(...).")
+      }
+      super.transformTemplate(tree)
     }
 
     override def transform(tree: Tree): Tree = tree match {
-      case tree@LabelDef(name, params, rhs) if (forwardJumps contains tree.symbol) =>
-        import scala.reflect.generic.Flags._
-        val valParams = params.map(x => ValDef(x.symbol))
-        val methodSymbol = currentTemplateSym.owner.newMethod(cunit.fresh.newName(tree.pos, name + "%"))
-        methodSymbol.setFlag(PRIVATE | FINAL | SYNTHETIC)
-        methodSymbol.setInfo(tree.symbol.tpe)
-        methodSymbol.initialize
-        val newRhs = transform(rhs)
-        //TODO we should supply privateWithin
-        val methodTree = mkDefDef(methodSymbol, Modifiers(methodSymbol.flags), valParams, newRhs)
-        val methodSelect = treeGen.mkAttributedRef(currentTemplateSym.owner.tpe, methodTree.symbol)
-        methodsForLabels += tree.symbol -> methodTree
-        mkApply(methodSelect, params)
-      case tree@Apply(fun: Ident, args) if (forwardJumps contains fun.symbol) =>
-        methodsForLabels.get(fun.symbol) match {
-          case Some(methodTree) =>
-            val methodSelect = treeGen.mkAttributedRef(currentTemplateSym.owner.tpe, methodTree.symbol)
-            mkApply(methodSelect, args)  
-          case None =>
-            Predef.error("methodsForLabel is broken")
-        }
-      case tree => super.transform(tree)
+      case tree@DefDef(mods, name, tparams, vparamss, tpt, _) if methodsToBeRewritten contains tree.symbol =>
+        val errorCall = localTyper typed gen.mkSysErrorCall("whack! you stepped on broken handling of jribble backend.")
+        treeCopy.DefDef(tree, mods, name, tparams, vparamss, tpt, errorCall)
+      case x => super.transform(x)
     }
 
     class FindLabelForwardJumpsTraverser extends Traverser {
@@ -106,5 +84,6 @@ trait RemoveForwardJumps extends Transform with JribbleNormalization {
       }
       def forwardJumps: Set[Symbol] = _forwardJumps.toSet
     }
+
   }
 }
